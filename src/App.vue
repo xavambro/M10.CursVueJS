46f43af8b84ca78b6028a6ad0db78b9e6588c195
xavambro <xavambro@gmail.com>
2021-03-04 15:54:11 +0100

    Creado proyecto, añadido bootstrap y creacion de carpetas

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,10 @@
   </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
+<script src="./scripts/app.js">
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './styles/app.scss'
 </style>
